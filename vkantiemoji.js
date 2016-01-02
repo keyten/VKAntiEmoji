76864ee79fcc72dbb4d79459550332cdafc7cd6b
Dmitriy <ikeyten@gmail.com>
2016-01-03 00:24:06 +0300

    Update vkantiemoji.js

--- vkantiemoji.js
+++ vkantiemoji.js
@@ -21,8 +21,12 @@
 		return;
 	}
 
+	if (!/https?:\/\/vk.com/.test(window.location.href)) {
+		return;
+	}
+
 	// the list: https://vk.com/emoji_vk
-	var emoji = [
+	var emojiList = [
 			':-)', ':-D', ';-)', 'xD', ';-P', ':-p', '8-)', 'B-)',
 			':-(', ';-]', '3(', ':\'(', ':_(', ':((', ':o', ':|',
 			'3-)', 'O:)', ';o', '8o', '8|', ':X', '<3', ':-*', '>(',
@@ -30,70 +34,32 @@
 		],
 
 		hiddenSymbol = 8292,
-		hiddenSymbol2 = 4448,
-		dotSymbol = 787,
-		topDotSymbol = 729,
-
-		modes = {
-			'hidden': function(text){
-				return emoji.forEach(function(letter){
-					text = text.split(letter).join(letter[0] + String.fromCharCode(hiddenSymbol) + letter.substring(1));
-				}), text;
-			},
-			// doesn't works in webkit (chrome, opera, maybe safari)
-			'hidden2': function(text){
-				return emoji.forEach(function(letter){
-					text = text.split(letter).join(letter[0] + String.fromCharCode(hiddenSymbol2) + letter.substring(1));
-				}), text;
-			},
-			'dot': function(text){
-				return emoji.forEach(function(letter){
-					text = text.split(letter).join(letter[0] + String.fromCharCode(dotSymbol) + letter.substring(1));
-				}), text;
-			},
-			'topDot': function(text){
-				return emoji.forEach(function(letter){
-					text = text.split(letter).join(String.fromCharCode(topDotSymbol) + letter);
-				}), text;
-			}
-		},
-		mode = window.localStorage.getItem('emojiDisableMode') || 'hidden',
-
-		css = window.localStorage.getItem('emojiDisableCSS') || true;
-
-	function emojiProcess(){
-
-		// xhr processing
-		var _send = window.XMLHttpRequest.prototype.send;
-		window.XMLHttpRequest.prototype.send = function(message){
-			if(!message || message.indexOf('msg=') == -1)
-				return _send.apply(this, arguments);
-			var text = decodeURIComponent(message.split('msg=')[1].split('&')[0]),
-				firstPart = message.split('msg=')[0],
-				lastPart = message.split('msg=')[1].split('&').slice(1).join('&');
-			var args = [].slice.call(arguments);
-			args[0] = (firstPart + 'msg=' + encodeURIComponent(modes[mode](text)) + '&' + lastPart);
-			return _send.apply(this, args);
+		hide = function(text){
+			console.log(text);
+			emojiList.forEach(function(letter){
+				text = text.split(letter).join(letter[0] + String.fromCharCode(hiddenSymbol) + letter.substring(1));
+			});
+			return text;
 		};
 
-		// client preprocessing
-		if(window.Emoji){
-			window.Emoji.getEmojiHTML = function(a){ return window.Emoji.cssEmoji[a][1]; };
-			window.Emoji.addEmoji = function(){};
+	// messages interception
+	var post = window.ajax.post;
+	window.ajax.post = function(url, params){
+		if(url === 'al_im.php' && params.act === 'a_send'){
+			params.msg = hide(params.msg);
 		}
-	}
+		return post.apply(this, arguments);
+	};
 
-	function cssProcess(){
-		var s = window.document.createElement('style');
-		s.textContent = '#im_rcemoji_cont { display: none; }';
-		window.document.body.appendChild(s);
-	}
+	// emoji
+	window.Emoji.getEmojiHTML = function(a){ return window.Emoji.cssEmoji[a][1]; };
+	window.Emoji.addEmoji = function(){};
+	window.Emoji.tabSwitch(document.getElementsByClassName('emoji_tab_-1')[0], -1, 0);
 
-	if (/https?:\/\/vk.com/.test(window.location.href)) {
-		emojiProcess();
-		if(css)
-			cssProcess();
-	}
-
+	// css
+	var style = window.document.createElement('style');
+	style.textContent = '.emoji_tab_0 { display: none; } ';
+	style.textContent += '#im_rcemoji_cont { display: none; }';
+	window.document.body.appendChild(style);
 
 })(window);
